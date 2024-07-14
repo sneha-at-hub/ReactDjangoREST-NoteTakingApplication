b69d5f41cd0c037832a31903d215d24de68fc721
sneha-at-hub <snehatmg2004@gmail.com>
2024-07-14 11:15:55 +0545

    Adding Serializer to query all notes at once

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -32,3 +34,10 @@ def getRoutes(request):
         }
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    note = Note.objects.all()
+    serializer = NoteSerializer(note, many=True)
+    return Response(serializer.data)
+    
