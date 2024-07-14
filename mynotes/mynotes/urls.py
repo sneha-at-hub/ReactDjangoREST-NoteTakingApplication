d494306adeab0a244a0d94c630c257f5b809e51f
sneha-at-hub <snehatmg2004@gmail.com>
2024-07-14 19:59:02 +0545

    adding hash routes so that there wont be conflict routes between react and django

--- mynotes/mynotes/urls.py
+++ mynotes/mynotes/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    # path('note/<>')
     
     
     
